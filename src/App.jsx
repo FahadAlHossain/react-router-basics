6d1f94a7dbadcc9fc965ee61ebe84baff5c9af31
FahadAlHossain <fahadalhossain5@gmail.com>
2025-08-07 16:11:00 +0600

    error handle

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,9 @@ import ContactForm from "./components/ContactForm";
 import NotFound from "./components/NotFound";
 import JobLayout from "./layout/JobLayout";
 import Jobs from "./pages/Jobs";
-import { JobDetailsLoader, jobsLoader } from './utilis/loadData';
+import { JobDetailsLoader, jobsLoader } from "./utilis/loadData";
 import JobDetails from "./components/JobDetails";
+import Error from "./components/Error";
 
 function App() {
   const router = createBrowserRouter(
@@ -29,9 +30,17 @@ function App() {
           <Route path="info" element={<ContactInfo></ContactInfo>}></Route>
           <Route path="form" element={<ContactForm></ContactForm>}></Route>
         </Route>
-        <Route path="jobs" element={<JobLayout></JobLayout>}>
+        <Route
+          path="jobs"
+          element={<JobLayout></JobLayout>}
+          errorElement={<Error></Error>}
+        >
           <Route index element={<Jobs></Jobs>} loader={jobsLoader}></Route>
-          <Route path=":id" element={<JobDetails></JobDetails>} loader={JobDetailsLoader}></Route>
+          <Route
+            path=":id"
+            element={<JobDetails></JobDetails>}
+            loader={JobDetailsLoader}
+          ></Route>
         </Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Route>
--- src/utilis/loadData.js
+++ src/utilis/loadData.js
@@ -1,10 +1,16 @@
 export const jobsLoader = async () => {
   const res = await fetch("http://localhost:5000/jobs");
+  if(!res.ok){
+    throw Error('Jobs could not found!!')
+  }
   return res.json();
 };
 
 export const JobDetailsLoader = async ({params})=> {
   const {id} = params;
   const res = await fetch("http://localhost:5000/jobs/" + id);
+  if(!res.ok){
+    throw Error('Could not found job details')
+  }
   return res.json();
 }
