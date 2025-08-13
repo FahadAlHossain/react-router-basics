b8d41cb8bf2fde0636924f341a29c663a8c719ac
FahadAlHossain <fahadalhossain5@gmail.com>
2025-08-13 16:36:28 +0600

    nothing changed

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -5,18 +5,14 @@ const Jobs = () => {
   const jobsData = useLoaderData();
   return (
     <div className="jobs">
-      {jobsData.map((job) => {
-        return (
-          <Link to={job.id.toString()} key={job.id}>
-            <h4>{job.title}</h4>
-            <p>{job.location}</p>
-          </Link>
-        );
-      })}
+      {jobsData.map((job) => (
+        <Link to={job.id.toString()} key={job.id}>
+          <h4>{job.title}</h4>
+          <p>{job.location}</p>
+        </Link>
+      ))}
     </div>
   );
 };
 
 export default Jobs;
-
-
